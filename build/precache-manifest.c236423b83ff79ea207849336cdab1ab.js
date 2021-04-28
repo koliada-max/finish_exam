735134296673279087727c48452902f1e09f1f7e
Nastya <nastya@Air-Nastya.lan>
2021-04-28 10:51:00 +0300

    Added exam project

---
+++ build/precache-manifest.c236423b83ff79ea207849336cdab1ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462adc969ef9a3d16eabeb9399f1898c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2978c552d7c12fb83e7f",
+    "url": "/static/css/main.e20a57bd.chunk.css"
+  },
+  {
+    "revision": "957f24288fb31644269b",
+    "url": "/static/js/2.ce2456d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ce2456d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2978c552d7c12fb83e7f",
+    "url": "/static/js/main.6847120a.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "fa65c7c85e446b81dd772c0d647c85f3",
+    "url": "/static/media/bar1.fa65c7c8.svg"
+  }
+]);
